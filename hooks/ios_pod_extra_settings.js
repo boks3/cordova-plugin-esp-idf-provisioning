eb862582d5e5c4ea2fdefc7ee78704345c9fff21
Quentin Farizon <quentin.farizon@gmail.com>
2022-11-29 15:17:46 +0100

    Simplify management of Swift version

--- hooks/ios_pod_extra_settings.js
+++ hooks/ios_pod_extra_settings.js
@@ -2,15 +2,6 @@ var fs = require('fs');
 
 var podExtraSettings = `
 ENV['SWIFT_VERSION'] = '5'
-use_frameworks!
-
-post_install do |installer|
-  installer.pods_project.targets.each do |target|
-    target.build_configurations.each do |config|
-      config.build_settings['SWIFT_VERSION'] = '5'
-    end
-  end
-end
 `;
 
 fs.appendFile('platforms/ios/Podfile', podExtraSettings, function (err, data) {
